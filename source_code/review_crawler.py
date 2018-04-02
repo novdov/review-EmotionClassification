68282734e1a6747f3045b107a47ff9522e4ff1a4
novdov <rpwjd56@gmail.com>
2018-04-03 01:25:15 +0900

    naver movie reviews cralwer

--- source_code/review_crawler.py
+++ source_code/review_crawler.py
@@ -1,3 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 import time
+
+def review_crawler():
+
+    nv_reviews = pd.DataFrame(columns=["title", "review"])
+
+    for i in tqdm_notebook(range(1, 1000+1)):
+        url = "https://movie.naver.com/movie/point/af/list.nhn?&page={}".format(i)
+        response = requests.get(url)
+        dom = BeautifulSoup(response.content, "html.parser")
+        dom.find_all("a", "movie")
+        reviews_pre = dom.find_all("td", "title")
+
+        for idx, rvw in enumerate(reviews_pre):
+            title = rvw.a.text
+            review = rvw.text.split('\n')[2].split(' \r')[0]
+            nv_reviews.loc[len(nv_reviews)] = {
+                "title": title,
+                "review": review
+            }
+
+        time.sleep(0.5)
+    return nv_reviews

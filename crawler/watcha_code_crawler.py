9e5d91d90ffe08533bacd07ad35bc9de53fa9d24
novdov <rpwjd56@gmail.com>
2018-04-15 22:17:33 +0900

    added code for title_url

--- crawler/watcha_code_crawler.py
+++ crawler/watcha_code_crawler.py
@@ -1,27 +1,19 @@
 import json
 import pandas as pd
-import requests
-
-# def make_url_evalmore(page, per):
-#     return "https://watcha.net/evalmore/category.json?" +\
-#            "page={}&per={}&category_idx=19".format(page, per)
-
-# url = make_url_evalmore(1, 601)
-#
-# response = requests.get(url)
-# json_data = response.json()
 
 with open('category.json') as f:
     json_data = json.loads(f.read())
 
 total_cards = json_data['cards']
-code_df = pd.DataFrame(columns=["code", "title"])
+code_df = pd.DataFrame(columns=["code", "title", "title_url"])
 
 for card in total_cards:
     code = card['items'][0]['item']['code']
     title = card['items'][0]['item']['title']
+    title_url = card['items'][0]['item']['title_url']
 
     code_df.loc[len(code_df)] = {
         "code": code,
-        "title": title
+        "title": title,
+        "title_url": title_url,
     }

aa4d7ed0f62aeee284c1af41b2f34406f7e2c00e
novdov <rpwjd56@gmail.com>
2018-04-17 11:51:22 +0900

    added docstring and error message

--- crawler/watcha_review_crawler.py
+++ crawler/watcha_review_crawler.py
@@ -5,7 +5,18 @@ import datetime
 from tqdm import tqdm
 
 
-def make_movie_url(unique_id, start, count):
+def make_movie_url(unique_id, start, count=10):
+    """
+    Generate url for requesting api of each movie.
+    Movies are listed in here. --> https://watcha.net/evalmore
+    :param unique_id: unique id of each movie
+    :param start: starting index of page
+    :param count: number of contents to be shown (max: 10)
+    :return: url for api of each movie
+    """
+    if count > 10:
+        print("Count can't exceed 10.")
+
     return "https://watcha.net/comment/list?unique_id=" + unique_id +\
            "&start_index=" + str(start) +\
            "&count=" + str(count) +\
@@ -13,13 +24,21 @@ def make_movie_url(unique_id, start, count):
 
 
 def review_crawler():
+    """
+    Get the review of requested movie.
+    :return: Dataframe with titles, reviews, and rating of movies.
+    """
+    # load csv file contains movie codes and the number of reviews of each movie
     df_tmp = pd.read_csv("../data/watcha_codes_page.csv")
+    # indices of page (total reviews // 10)
     page_split = df_tmp["page_split"]
+    # unique id of movies
     codes = df_tmp["code"]
     zipped = zip(codes, page_split)
 
     df = pd.DataFrame(columns=["title", "review", "rating"])
 
+    # number of contents to be shown for each iteration.
     count = 10
     try:
         for code, page in zipped:
